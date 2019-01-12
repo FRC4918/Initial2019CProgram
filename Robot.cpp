8e95a6a2d300a0f02f32dfda13d4bc21cc7fa78b
bsdjeff <jgibbons@n2.net>
2019-01-11 19:29:15 -0800

    Added code to control another motor, use PID, and add limelight.
    Added code to control an additional motor with PID algorithm on
    the Talon SRC controller, using a magnetic encoder on the output
    shaft (after 2 7:1 gearbox reduction stages), and to use
    Network Tables to retrieve values from a limelight video camera.

--- Robot.cpp
+++ Robot.cpp
@@ -1,29 +1,37 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
+/* Team 4918 Robot                                                            */
 /*----------------------------------------------------------------------------*/
 
-#include <IterativeRobot.h>
-#include <Joystick.h>
-#include <Spark.h>
+#include <frc/WPILib.h>
 #include "ctre/Phoenix.h"
-#include <Compressor.h>
-#include <DoubleSolenoid.h>
-#include <DriverStation.h>
-#include <Drive/DifferentialDrive.h>
 #include <iostream>
+#include <networktables/NetworkTable.h>
+#include <networktables/NetworkTableEntry.h>
+#include <networktables/NetworkTableInstance.h>
+
+// all the following are already included by frc/WPILib.h above
+// #include <frc/TimedRobot.h>
+// #include <frc/Joystick.h>
+// #include <frc/Spark.h>
+// #include <frc/Compressor.h>
+// #include <frc/DoubleSolenoid.h>
+// #include <frc/DriverStation.h>
+// #include <frc/drive/DifferentialDrive.h>
+// #include <frc/AnalogInput.h>
+
+using std::cout;
+using std::endl;
 
 /**
- * This sample program shows how to control a motor using a joystick. In the
- * operator control part of the program, the joystick is read and the value is
- * written to the motor.
+ * This sample program shows how to control several motors using a joystick.
+ * In the operator control part of the program, the joystick is read and the
+ * value is used to determine how to drive the motor.
  *
- * Joystick analog values range from -1 to 1 and speed controller inputs as
- * range from -1 to 1 making it easy to work together.
+ * Joystick analog values range from -1 to 1, and speed controller inputs of
+ * m_motor*.Set() also range from -1 to 1, so those 2 work together easily;
+ * other motor drive methods require some scaling.
  */
-class Robot : public frc::IterativeRobot {
+class Robot : public frc::TimedRobot {
    public:
       void AutonomousInit() override {
          iAutoCount = 0;
@@ -31,8 +39,12 @@ class Robot : public frc::IterativeRobot {
          m_motorLSSlave2.Follow(m_motorLSMaster);
          m_motorRSSlave1.Follow(m_motorRSMaster);
          m_motorRSSlave2.Follow(m_motorRSMaster);
-         m_compressor.SetClosedLoopControl(true); // turn compresser on
-         m_doublesolenoid.Set(frc::DoubleSolenoid::Value::kForward);
+#ifdef JAG_NOTDEFINED
+         m_compressor.SetClosedLoopControl(true);  // turn compresser on
+#else
+         m_compressor.SetClosedLoopControl(false); // turn compressor off
+#endif
+         m_doublesolenoid.Set(frc::DoubleSolenoid::Value::kForward); // low gear
          m_motorArmMaster.ConfigNominalOutputForward(0, 10);
          m_motorArmMaster.ConfigNominalOutputReverse(0, 10);
          m_motorArmMaster.ConfigPeakOutputForward(1, 10);
@@ -45,13 +57,13 @@ class Robot : public frc::IterativeRobot {
          } else if ( iAutoCount < 200 ) {
            m_drive.CurvatureDrive( 0.5, 0.5, 0 );
          } else if ( iAutoCount < 201 ) {
-           m_doublesolenoid.Set(frc::DoubleSolenoid::Value::kReverse);
+           m_doublesolenoid.Set(frc::DoubleSolenoid::Value::kReverse);  // high gear
          } else if ( iAutoCount < 600 ) {
            m_drive.CurvatureDrive( 0.5, -0.5, 0 );
          } else if ( iAutoCount < 700 ) {
            m_drive.CurvatureDrive( 0.5, 0.5, 0 );
       // } else if ( iAutoCount < 700 ) {
-      //   m_drive.CurvatureDrive( 0.5, 0.5, 0 );                                       }
+      //   m_drive.CurvatureDrive( 0.5, 0.5, 0 );
          } else {
            m_drive.CurvatureDrive( 0.0, 0.0, 0 );
          }
@@ -63,21 +75,42 @@ class Robot : public frc::IterativeRobot {
          m_motorLSSlave2.Follow(m_motorLSMaster);
          m_motorRSSlave1.Follow(m_motorRSMaster);
          m_motorRSSlave2.Follow(m_motorRSMaster);
-         m_doublesolenoid.Set(frc::DoubleSolenoid::Value::kForward);
+         m_doublesolenoid.Set(frc::DoubleSolenoid::Value::kForward); // low gear
          m_motorArmMaster.ConfigNominalOutputForward(0, 10);
          m_motorArmMaster.ConfigNominalOutputReverse(0, 10);
          m_motorArmMaster.ConfigPeakOutputForward(1, 10);
          m_motorArmMaster.ConfigPeakOutputReverse(-1,10);
+                   // set ramp speed to 1.0 seconds from Neutral to Full power
+         m_motorArmMaster.ConfigClosedloopRamp( 1.0, 10 );
+
+         m_motorArmMaster.ConfigPeakCurrentLimit( 50, 10 );  // 50 Amps
+         m_motorArmMaster.ConfigPeakCurrentDuration( 200, 10 );  // 200 msecs
+         m_motorArmMaster.ConfigContinuousCurrentLimit( 40, 10 );  // 40 Amps
+         m_motorArmMaster.EnableCurrentLimit( true );
+
          m_motorArmMaster.ConfigSelectedFeedbackSensor( FeedbackDevice::CTRE_MagEncoder_Relative,
                                                         0, 0 );
          m_motorArmMaster.SetSensorPhase( true );
-         m_motorArmMaster.Config_kF( 0, 0.45600, 0 );
-         m_motorArmMaster.Config_kP( 0, 0.6, 0 );
-         m_motorArmMaster.Config_kI( 0, 0.006, 0 );
-         m_motorArmMaster.Config_kD( 0, 0.6, 0 );
+         m_motorArmMaster.Config_kF( 0, 0.40000, 0 );   // was 0.456
+         m_motorArmMaster.Config_kP( 0, 0.8, 0 );
+         m_motorArmMaster.Config_kI( 0, 0.0, 0 ); // if used, make this
+                                                  // very small, like 0.006
+         m_motorArmMaster.Config_kD( 0, 1.0, 0 );
+
+         DistSensor1.SetOversampleBits( 4 );
+         DistSensor1.SetAverageBits( 2 );
+         DistSensor1.SetSampleRate( 3200 );
       }
 
       void TeleopPeriodic() override {
+
+         static int iCallCount = 0;
+         double limex = limenttable->GetNumber("tx",0.0);
+         double limey = limenttable->GetNumber("ty",0.0);
+         double limev = limenttable->GetNumber("tv",0.0);
+
+         iCallCount++;
+              
          // jagorigwas: m_motor.Set(m_stick.GetY());
          // m_motorRSMaster.Set(ControlMode::PercentOutput, m_stick.GetY());
          // m_motorLSMaster.Set(ControlMode::PercentOutput, m_stick.GetY());
@@ -94,38 +127,94 @@ class Robot : public frc::IterativeRobot {
                                        m_stick.GetTop() );m_compressor.SetClosedLoopControl(false);
             }
          }
-          m_compressor.SetClosedLoopControl(true); // turn compressor on
-          // Compressor *c = new Compressor(0);
-          // m_doublesolenoid.Set(DoubleSolenoid.Value.kOff);
-          if ( m_stick.GetTrigger() ) {
-             m_doublesolenoid.Set(frc::DoubleSolenoid::Value::kReverse); // high gear
-          } else {
-             m_doublesolenoid.Set(frc::DoubleSolenoid::Value::kForward); // low gear
-          }
-          // m_doublesolenoid.Set(DoubleSolenoid::Value::kReverse);
-          if ( !m_stick.GetRawButton(4) ) {
-              /* Speed mode */
-              /*
-               * 4096 Units/Rev * 500 RPM / 600 100ms/min in either direction:
-               * velocity setpoint is in units/100ms
-               */
-              double targetVelocity_UnitsPer100ms = m_stick.GetZ() * 2240;
-              /* 1500 RPM in either direction */
-              if ( m_stick.GetRawButton(5) ) {
-                 std::cout << "button5 " << std::endl;
-                 m_motorArmMaster.Set( ControlMode::Velocity, -1000 );
-              } else if ( m_stick.GetRawButton(6) ) {
-                 std::cout << "button6 " << std::endl;
-                 m_motorArmMaster.Set( ControlMode::Velocity, 1000 );
-              } else {
-                m_motorArmMaster.Set( ControlMode::Velocity, targetVelocity_UnitsPer100ms);
-                std::cout << "Velocity " << targetVelocity_UnitsPer100ms << "/" << m_motorArmMaster.GetSelectedSensorVelocity(0);
-                std::cout << " ( "   << m_motorArmMaster.GetSelectedSensorVelocity(0) - targetVelocity_UnitsPer100ms << " )" << std::endl;
-              }
+#ifdef JAG_NOTDEFINED
+         m_compressor.SetClosedLoopControl(true); // turn compressor on
+#else
+         m_compressor.SetClosedLoopControl(false); // turn compressor off
+#endif
+         // Compressor *c = new Compressor(0);
+         // m_doublesolenoid.Set(DoubleSolenoid.Value.kOff);
+         if ( m_stick.GetTrigger() ) {
+            m_doublesolenoid.Set(frc::DoubleSolenoid::Value::kReverse); // high gear
+         } else {
+            m_doublesolenoid.Set(frc::DoubleSolenoid::Value::kForward); // low gear
+         }
+         // m_doublesolenoid.Set(DoubleSolenoid::Value::kReverse); // high gear
+         if ( !m_stick.GetRawButton(4) ) {
+            /* Speed mode */
+            /*
+             * 4096 Units/Rev * 328 RPM / 600 100ms/min in either direction:
+             * velocity setpoint is in units/100ms
+             * We found the max speed of this motor/gearbox combo to be
+             * about 2240 ticks/100ms, or about 328 RPM.
+             */
+            double targetVelocity_UnitsPer100ms = 0.0; 
 
-          } else {
-              m_motorArmMaster.Set(ControlMode::PercentOutput, m_stick.GetZ());
-          }
+            if ( m_stick.GetRawButton(5) ) {
+               // cout << "button5 " << endl;
+               // exactly 146 RPM to the left (-1000 ticks/100ms)
+               targetVelocity_UnitsPer100ms = -1000;
+            } else if ( m_stick.GetRawButton(6) ) {
+               // cout << "button6 " << endl;
+               // exactly 146 RPM to the right (1000 ticks/100ms)
+               targetVelocity_UnitsPer100ms = 1000.0;
+            } else {
+                     /* up to 328 RPM (2240 ticks/100ms) in either direction */
+               targetVelocity_UnitsPer100ms = m_stick.GetZ() * 4096 * 328.0 / 600;
+            }
+            if (limev == 1){
+               if (limex >= 0.0){
+                  targetVelocity_UnitsPer100ms = 750;
+               } else {
+                  targetVelocity_UnitsPer100ms = -750;
+               }
+               if (limey >= 0.0){
+                  targetVelocity_UnitsPer100ms = targetVelocity_UnitsPer100ms * 2;
+               } else {
+                  targetVelocity_UnitsPer100ms = targetVelocity_UnitsPer100ms * 0.5;
+               } 
+            }
+
+
+
+               // Display actual/desired speed every 10 iterations
+            if ( 0 == (iCallCount%10) )
+            {
+               m_motorArmMaster.Set( ControlMode::Velocity, targetVelocity_UnitsPer100ms);
+
+               //cout << "speed=";
+// #ifdef JAG_NOTDEFINED
+               cout << cout.fill(' ');
+               cout << cout.width(6) << (int)m_motorArmMaster.GetSelectedSensorVelocity(0);
+               cout << "/";
+               cout << cout.width(6) << (int)targetVelocity_UnitsPer100ms;
+               cout << ", (";
+               cout << cout.width(6) << (int)targetVelocity_UnitsPer100ms
+                                        - (int)m_motorArmMaster.GetSelectedSensorVelocity(0);
+               cout << ", ";
+               cout << cout.width(6) << m_motorArmMaster.GetMotorOutputPercent();
+               cout << "% )";
+               cout << "|";
+// #endif
+            }
+         } else {
+            m_motorArmMaster.Set(ControlMode::PercentOutput, m_stick.GetZ());
+         }
+
+         if ( 1 == (iCallCount%10) ) {
+            //cout << " Dist=";
+            //cout << DistSensor1.GetValue();
+            // cout << "/";
+            // cout << DistSensor1.GetAverageValue();
+            // cout << " ( ";
+            // cout << DistSensor1.GetVoltage();
+            // cout << "/";
+            // cout << DistSensor1.GetAverageVoltage();
+            // cout << " )";
+            cout << limenttable->GetNumber("tx",0.0);
+            cout << endl;
+	      }
+
       }
 
    private:
@@ -140,9 +229,14 @@ class Robot : public frc::IterativeRobot {
       WPI_VictorSPX m_motorLSSlave2{5};
       WPI_VictorSPX m_motorRSSlave2{6};
       int iAutoCount;
-      DifferentialDrive m_drive{ m_motorLSMaster, m_motorRSMaster };
-      Compressor m_compressor{0};
-      DoubleSolenoid m_doublesolenoid{0,1};
+      frc::DifferentialDrive m_drive{ m_motorLSMaster, m_motorRSMaster };
+      frc::Compressor m_compressor{0};
+      frc::DoubleSolenoid m_doublesolenoid{0,1};
+      frc::AnalogInput DistSensor1{0};
+      std::shared_ptr<NetworkTable> limenttable = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
 };
 
-START_ROBOT_CLASS(Robot)
+// START_ROBOT_CLASS(Robot)
+
+int main() { return frc::StartRobot<Robot>(); }
+
